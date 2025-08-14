6086f4300e35ac0a3b5c72ee9b2046fa664f22a2
Michael <100490812+DebSol11@users.noreply.github.com>
2025-08-14 11:18:43 +0200

    Working sweating wheel

---
+++ src/createToDos.js
@@ -0,0 +1,37 @@
+export const greeting = "Hello, Odinite!"
+
+export let toDoObject = {
+    title: "Train",
+    description: "Do some form of physical activity during the day, may it be with or without a specific training goal",
+    dueDate: "every day",
+    priority: "AAA",
+    notes: "To raise to the sky",
+    checklist: true,
+}
+
+let arrayOfToDoObjects = [
+]
+
+// REFACTORING CONSTRUCTOR INTO FACTORY FUNCTION
+const ToDoObject = function (title, description, dueDate, priority, notes, checklist) {
+  this.title = title;
+  this.description = description;
+  this.dueDate = dueDate;
+  this.priority = priority;
+  this.notes = notes;
+  this.checklist = checklist;
+}
+// hey, this is a constructor - 
+// then this can be refactored into a factory!
+
+function createToDoObject (title, description, dueDate, priority, notes, checklist) {
+  return { title, description, dueDate, priority, notes, checklist };
+}
+// and that's very similar, except since it's just a function,
+// we don't need a new keyword
+
+const toDo1 = createToDoObject("Fart", "A compression of air exiting the after thereby making a noticeable sound", "01.01.0101", "AAA", "Only do the fart if the risk of a wet fart does not exceed 50%", "false");
+
+console.log(toDo1);
+console.log(toDo1.title);
+console.log(toDo1.notes);
---
+++ src/script.js
@@ -0,0 +1,6 @@
+import "./styles.css"
+import { greeting } from "./createToDos.js"
+import { toDoObject } from "./createToDos.js";
+
+console.log(greeting);
+console.log(toDoObject);
