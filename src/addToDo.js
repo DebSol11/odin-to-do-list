a5967ba1105956db83dab90be3800a8cdd9bbc4a
Michael <100490812+DebSol11@users.noreply.github.com>
2025-08-24 21:40:42 +0200

    Work on add to Do modal functionality

--- src/addToDo.js
+++ src/addToDo.js
@@ -1,16 +1,33 @@
-function displayToDoButton() {
-  const divContentWrap = document.querySelector(".content-wrap")
-  let toDoBtn = document.createElement("button");
-  toDoBtn.textContent = "Add to do";
-  toDoBtn.setAttribute("id", "toDoBtn");
-  divContentWrap.appendChild(toDoBtn);
-}
+// function displayToDoButton() {
+//   const divContentWrap = document.querySelector(".content-wrap")
+//   let toDoBtn = document.createElement("button");
+//   toDoBtn.textContent = "Add to do";
+//   toDoBtn.setAttribute("id", "toDoBtn");
+//   divContentWrap.appendChild(toDoBtn);
+// }
+
+const addToDoFormModal = document.getElementById("addToDoFormModal");
 
 function listenForAddToDoBtnClick() {
-  const toDoBtn = document.querySelector("#toDoBtn")
-  toDoBtn.addEventListener("click", () => {
-    
+  const addToDoIcon = document.querySelector("#addToDoIcon");
+  const submitBtn = document.getElementById("submitBtn");
+  addToDoIcon.addEventListener("click", () => {
+    console.log("Works");
+    addToDoFormModal.classList.add("active");
+    addToDoFormModal.classList.add("open");
+    addToDoFormModal.style.display = "block";
   });
 }
 
-export { displayToDoButton, listenForAddToDoBtnClick };
+function listenForCloseModalClick() {
+  const closeModal = document.getElementById("closeModal");
+  closeModal.addEventListener("click", () => {
+    addToDoFormModal.classList.add("slideOut");
+    setTimeout(() => {
+      addToDoFormModal.classList.remove("active", "slideOut");
+      addToDoFormModal.style.display = "none";
+    }, 500); // Wait for the animation to complete (500ms)
+  });
+}
+
+export { listenForAddToDoBtnClick, listenForCloseModalClick };
