09cd2dc8a0e0460dd4e3f64be785ebf6a2a9b12a
Michael <100490812+DebSol11@users.noreply.github.com>
2025-08-29 16:29:35 +0200

    Styling the app and simplifying the user interface

--- src/detailsBtn.js
+++ src/detailsBtn.js
@@ -3,15 +3,16 @@ import { arrayOfToDoObjects } from "./createToDos.js";
 const displayDetailsModal = document.getElementById("displayDetailsModal");
 const toDoDetails = document.getElementById("toDoDetails");
 
-
 function displayDetails(index) {
   let keysArray = Object.keys(arrayOfToDoObjects[index]);
   console.log(keysArray);
 
   const projectContainer = document.createElement("div");
+  projectContainer.classList.add("detailsGrid");
   const projectKey = document.createElement("h3");
   projectKey.classList.add("displayInline");
-  projectKey.textContent = keysArray[0].charAt(0).toUpperCase() + keysArray[0].slice(1) +": ";
+  projectKey.textContent =
+    keysArray[0].charAt(0).toUpperCase() + keysArray[0].slice(1) + ": ";
   projectContainer.appendChild(projectKey);
   const projectPara = document.createElement("p");
   projectPara.classList.add("displayInline");
@@ -19,60 +20,70 @@ function displayDetails(index) {
   projectContainer.appendChild(projectPara);
   toDoDetails.appendChild(projectContainer);
 
+  const priorityContainer = document.createElement("div");
+  priorityContainer.classList.add("detailsGrid");
   const priorityKey = document.createElement("h3");
   priorityKey.classList.add("displayInline");
-  priorityKey.textContent = keysArray[1].charAt(0).toUpperCase() + keysArray[1].slice(1) +": ";
-  toDoDetails.appendChild(priorityKey);
+  priorityKey.textContent =
+    keysArray[1].charAt(0).toUpperCase() + keysArray[1].slice(1) + ": ";
+  priorityContainer.appendChild(priorityKey);
   const priorityPara = document.createElement("p");
   priorityPara.classList.add("displayInline");
   priorityPara.textContent = arrayOfToDoObjects[index].priority;
-  toDoDetails.appendChild(priorityPara);
-  const breakLine1 = document.createElement("br")
-  toDoDetails.appendChild(breakLine1);
+  priorityContainer.appendChild(priorityPara);
+  toDoDetails.appendChild(priorityContainer);
 
+  const titleContainer = document.createElement("div");
+  titleContainer.classList.add("detailsGrid");
   const titleKey = document.createElement("h3");
   titleKey.classList.add("displayInline");
-  titleKey.textContent = keysArray[2].charAt(0).toUpperCase() + keysArray[2].slice(1) +": ";
-  toDoDetails.appendChild(titleKey);
+  titleKey.textContent =
+    keysArray[2].charAt(0).toUpperCase() + keysArray[2].slice(1) + ": ";
+  titleContainer.appendChild(titleKey);
   const titlePara = document.createElement("p");
   titlePara.classList.add("displayInline");
   titlePara.textContent = arrayOfToDoObjects[index].title;
-  toDoDetails.appendChild(titlePara);
-  const breakLine2 = document.createElement("br")
-  toDoDetails.appendChild(breakLine2);
+  titleContainer.appendChild(titlePara);
+  toDoDetails.appendChild(titleContainer);
 
+  const descriptionContainer = document.createElement("div");
+  descriptionContainer.classList.add("detailsGrid");
   const descriptionKey = document.createElement("h3");
   descriptionKey.classList.add("displayInline");
-  descriptionKey.textContent = keysArray[3].charAt(0).toUpperCase() + keysArray[3].slice(1) +": ";
-  toDoDetails.appendChild(descriptionKey);
+  descriptionKey.textContent =
+    keysArray[3].charAt(0).toUpperCase() + keysArray[3].slice(1) + ": ";
+  descriptionContainer.appendChild(descriptionKey);
   const descriptionPara = document.createElement("p");
   descriptionPara.classList.add("displayInline");
   descriptionPara.textContent = arrayOfToDoObjects[index].description;
-  toDoDetails.appendChild(descriptionPara);
-  const breakLine3 = document.createElement("br")
-  toDoDetails.appendChild(breakLine3);
+  descriptionContainer.appendChild(descriptionPara);
+  toDoDetails.appendChild(descriptionContainer);
 
+  const dueDateContainer = document.createElement("div");
+  dueDateContainer.classList.add("detailsGrid");
   const dueDateKey = document.createElement("h3");
   dueDateKey.classList.add("displayInline");
-  dueDateKey.textContent = keysArray[4].charAt(0).toUpperCase() + keysArray[4].slice(1) +": ";
-  toDoDetails.appendChild(dueDateKey);
+  dueDateKey.textContent =
+    keysArray[4].charAt(0).toUpperCase() + keysArray[4].slice(1) + ": ";
+  dueDateContainer.appendChild(dueDateKey);
   const dueDatePara = document.createElement("p");
   dueDatePara.classList.add("displayInline");
   dueDatePara.textContent = arrayOfToDoObjects[index].dueDate;
-  toDoDetails.appendChild(dueDatePara);
-  const breakLine4 = document.createElement("br")
-  toDoDetails.appendChild(breakLine4);
+  dueDateContainer.appendChild(dueDatePara);
+  toDoDetails.appendChild(dueDateContainer);
 
+  const notesContainer = document.createElement("div");
+  notesContainer.classList.add("detailsGrid");
   const notesKey = document.createElement("h3");
   notesKey.classList.add("displayInline");
-  notesKey.textContent = keysArray[5].charAt(0).toUpperCase() + keysArray[5].slice(1) +": ";
-  toDoDetails.appendChild(notesKey);
+  notesKey.textContent =
+    keysArray[5].charAt(0).toUpperCase() + keysArray[5].slice(1) + ": ";
+  notesContainer.appendChild(notesKey);
   const notesPara = document.createElement("p");
   notesPara.classList.add("displayInline");
   notesPara.textContent = arrayOfToDoObjects[index].notes;
-  toDoDetails.appendChild(notesPara);
-  const breakLine5 = document.createElement("br")
-  toDoDetails.appendChild(breakLine5);
+  notesContainer.appendChild(notesPara);
+  toDoDetails.appendChild(notesContainer);
 }
 
 function listenForDetailsBtnClick() {
@@ -105,7 +116,5 @@ function clearChildNotes(parentNode) {
     parentNode.removeChild(parentNode.firstChild);
   }
 }
-
-
 
 export { listenForDetailsBtnClick, listenForCloseDetailsClick };
