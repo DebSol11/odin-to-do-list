e7ea419311524563b7bd0c3f3e0e50cd9f4be914
Michael <100490812+DebSol11@users.noreply.github.com>
2025-09-04 13:35:20 +0200

    Final commit before merge into main

--- src/.js/projectFilterToDos.js
+++ src/.js/projectFilterToDos.js
@@ -1,6 +1,7 @@
 import { arrayOfToDoObjects } from "./createToDos";
 import { displayToDos } from "./displayToDos";
 import { clearChildNotes } from "./detailsBtn";
+import { updateProjects } from "./addProjects";
 
 function filterArray(forProject) {
   let newArray = arrayOfToDoObjects.filter((obj) => obj.project == forProject);
@@ -13,12 +14,17 @@ function listenForProjectLiClick() {
   for (let i = 0; i < projectNodeList.length; i++) {
     projectNodeList[i].addEventListener("click", () => {
       let newArray = filterArray(projectNodeList[i].id);
-      const toDoContainer = document.getElementById("toDoContainer2");
-      clearChildNotes(toDoContainer);
-      displayToDos(newArray);
-      return newArray;
+  const toDoContainer = document.getElementById("toDoContainer2");
+  clearChildNotes(toDoContainer);
+  displayToDos(newArray);
+  return newArray;
     });
   }
 }
+
+// function displayFilteredArray(i) {
+//   const projectNodeList = document.querySelectorAll("li");
+  
+// }
 
 export { listenForProjectLiClick };
--- src/.js/script.js
+++ src/.js/script.js
@@ -23,3 +23,10 @@ removeDuplicateElementsFromArray((getValuesOfObjectArrayKey(arrayOfToDoObjects, 
 
 
 // compared to removing, simplifying and increasing speed, adding extra functionality is a piece of cake
+
+// "Bugs" to fix:
+// - When pressed an a project all the available Projects should still be shown, not only the pressed one.
+// - When a specific project is selected and a new to do for it is created and added it should only display the to does of the selected project but add the to do to an array where all the to does reside 
+// - Same logic as the above bullet point should apply when a to do is edited. It should add the edited to do to an array of objects where all the to does are in but only display the previously selected project to does
+// - The numbers after the projects should update according to the deleted to does
+// - Last if a project is added new all the above should also work on that new project
